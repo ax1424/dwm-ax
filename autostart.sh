4c27e8028ae8531848858de55c67f139aa2ca476
ax1424 <astonxavier5@gmail.com>
2024-07-11 19:52:27 +0800

    added my dwm build

---
+++ autostart.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash 
+
+### AUTOSTART PROGRAMS ###
+copyq & 
+dwmblocks &
+#flameshot & 
+lxsession & 
+mpv --no-video ~/Music/startup.mp3 & 
+nm-applet & 
+picom --daemon & 
+redshift &
+volumeicon & 
+xfce4-power-manager &
+
+### UNCOMMENT ONLY ONE OF THE TWO OPTIONS! ###
+#1.Restore the Last Wallpaper
+nitrogen --restore & 
+#2.Get a Random Wallpaper
+#nitrogen --set-zoom-fill --random ~/wallpapers & 
